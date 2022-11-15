008415ee7b17635b3d81431ea804e216eaf0e5be
popova1 <alexander.popov@aalto.fi>
2022-11-15 17:47:14 +0200

    Some gosq basics

--- cmd/gosq/main.go
+++ cmd/gosq/main.go
@@ -1,10 +1,30 @@
 package main
 
 import (
+    "flag"
+    "fmt"
+
     "github.com/dexter3k/go-squirrel/sqvm"
 )
 
+var (
+    showVersionInfo = flag.Bool("v", false, "display version info")
+)
+
+func init() {
+    flag.Usage = func() {
+        fmt.Printf("Usage of gosq: [OPTIONS] [program.nut [ARGS]]\n")
+        flag.PrintDefaults()
+    }
+    flag.Parse()
+}
+
 func main() {
+    if *showVersionInfo {
+        fmt.Printf("go-squirrel wip\n")
+        return
+    }
+
     sq := sqvm.Open(1024)
     defer sq.Close()
 
@@ -16,4 +36,15 @@ func main() {
     )
 
     sq.PushRootTable()
+
+    // Register libraries
+
+    // Register error handlers
+
+    if len(flag.Args()) == 0 {
+        fmt.Printf("Interpreter mode is not yet implemented")
+        return
+    } else {
+        // Execute the file...
+    }
 }
